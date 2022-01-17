f0049ac6b38192819bb311a3a2bfe1ba6e7ade61
MrPink925 <77735965+MrPink925@users.noreply.github.com>
2022-01-17 11:54:15 -0500

    change html page

--- test.ps1
+++ test.ps1
@@ -11,7 +11,8 @@ function GenerateFile {
     $Header = @"
     <style>
         body {
-            background-color: #1c1813
+            background-color: #1c1813;
+            font-family: 'Montserrat', sans-serif;
         }
         .inLineText{
             display: flex;
@@ -20,13 +21,29 @@ function GenerateFile {
         }
         div{
             display: flex;
-            flex-direction: column;
+            flex-direction: row;
             margin: 10vh;
+            margin-top: 2vh;
+            flex-wrap: wrap;
+        }
+        .table{
+            margin: 0;
+        }
+        .info{
+            margin: 0;
+            margin-right: 1vh;
+            display:flex;
+            flex-direction: column;
+        }
+        .title{
+            margin: 0;
+            margin-top:10vh;
+            margin-left: 10vh;
         }
         h1 {
     
             font-family: 'Montserrat', sans-serif;
-            color: #1ed760;
+            color: #4283b9;
             font-size: 32px;
     
         }
@@ -34,64 +51,62 @@ function GenerateFile {
         h2 {
     
             font-family: 'Montserrat', sans-serif;
-            color: #ffffff;
+            color: white;
             font-size: 19px;
     
         }
     
         h3 {
             font-family: 'Montserrat', sans-serif;
-            color: #ffffff;
+            color: white;
             font-size: 17px;
             margin-right:5px;
             font-weight: light;
         }
-    
-       table {
-            font-size: 12px;
-            border: 0px; 
-            font-family: Arial, Helvetica, sans-serif;
-        } 
-        
-        td {
-            padding: 4px;
-            margin: 0px;
-            border: 0;
-        }
-        
-        th {
-            background: #395870;
-            background: linear-gradient(#49708f, #293f50);
-            color: #fff;
-            font-size: 11px;
-            text-transform: uppercase;
-            padding: 10px 15px;
-            vertical-align: middle;
-        }
-    
-        tbody tr:nth-child(even) {
-            background: #f0f0f2;
-        }
         
         #CreationDate {
-    
-            font-family: Arial, Helvetica, sans-serif;
             color: #ff3300;
             font-size: 12px;
     
         }
-    
-        .StopStatus {
-    
-            color: #ff0000;
-        }
         
-      
-        .RunningStatus {
-    
-            color: #008000;
+        .styled-table {
+            border-collapse: collapse;
+            margin: 25px 0;
+            font-size: 0.9em;
+            min-width: 400px;
+            box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
         }
-    
+
+        .styled-table thead tr {
+            background-color: #4283b9;
+            color: #ffffff;
+            text-align: left;
+        }
+
+        .styled-table th,
+        .styled-table td {
+            padding: 12px 15px;
+        }
+
+        .styled-table tbody tr {
+            border-bottom: 1px solid #dddddd;
+        }
+
+
+        .styled-table tbody tr:nth-of-type(odd) {
+            background-color: #a5d3f5;
+        }
+        .styled-table tbody tr:nth-of-type(even) {
+            background-color: #4283b9;
+            
+        }
+
+        .styled-table tbody tr:last-of-type {
+            border-bottom: 2px solid;
+        }
+
+            
     </style>
     
 "@
@@ -123,25 +138,64 @@ function GenerateFile {
      $User_Office = Get-ADUser -Identity $DesiredUser -Properties office|
      Select-Object -ExpandProperty office
 
+     $User_ManagerTemp = Get-ADUser -Identity $DesiredUser -Properties Manager|
+     Select-Object -ExpandProperty Manager
+
+     $User_Manager = Get-ADUser -Identity $User_ManagerTemp -Properties DisplayName|
+     Select-Object -ExpandProperty DisplayName
+
+     $Groups = Get-ADPrincipalGroupMembership -Identity $DesiredUser | Select-Object Name 
+
+
+     $assignToPostContent = "<div class = 'info'><div class='inLineText'><h3>Username:  </h3> <h2>" + $User_Name + "</h2></div>" + 
+     "<div class='inLineText'><h3>Email:  </h3> <h2> " + $User_Email + "</h2></div>" +
+     "<div class='inLineText'><h3>Full Name:  </h3> <h2> " + $User_FullName + "</h2></div>" +
+     "<div class='inLineText'><h3>Description:  </h3> <h2> " + $User_Desc + "</h2></div>" +
+     "<div class='inLineText'><h3>Department:  </h3> <h2> " + $User_Department + "</h2></div>"+
+     "<div class='inLineText'><h3>Job Title:  </h3> <h2> " + $User_JobTitle + "</h2></div>"+
+     "<div class='inLineText'><h3>Company:  </h3> <h2> " + $User_Company+ "</h2></div>" +
+     "<div class='inLineText'><h3>Office:  </h3> <h2> " + $User_Office+ "</h2></div>" +
+     "<div class='inLineText'><h3>Manager:  </h3> <h2> " + $User_Manager+ "</h2></div></div>"
+
+    $assignToPostContent += "<div class='table'>
+	<table class = 'styled-table'>
+		<thead>
+			<tr>
+				<th>Security Groups on User</th>
+			</tr>
+		</thead>
+		<tbody>
+"
+foreach ($g in $Groups.GetEnumerator() )
+      {
+          #Write-Host $g.name
+           $nameOfGroup = $g.name
+          $assignToPostContent += "
+			<tr>
+                <td>$nameOfGroup name`</td>
+			</tr>
+			
+		"
+      }
+
+$assignToPostContent += "
+		</tbody>
+	</table></div>
+</div>
+"
+
      #Create the hashtable that will structure the html page
     $htmlParams = @{
-        Head = $Header 
-        Title = "This is Description"
-        Body = "<link rel='preconnect' href='https://fonts.googleapis.com'>
+        Head = $Header +"<link rel='preconnect' href='https://fonts.googleapis.com'>
         <link rel='preconnect' href='https://fonts.gstatic.com' crossorigin>
-        <link href='https://fonts.googleapis.com/css2?family=Montserrat&display=swap' rel='stylesheet'>
-    
-        <div> <h1>Generated by Corporate IT</h1>"
-        PostContent = "<div class='inLineText'><h3>Username:  </h3> <h2>" + $User_Name + "</h2></div>" + 
-        "<div class='inLineText'><h3>Email:  </h3> <h2> " + $User_Email + "</h2></div>" +
-        "<div class='inLineText'><h3>Full Name:  </h3> <h2> " + $User_FullName + "</h2></div>" +
-        "<div class='inLineText'><h3>Description:  </h3> <h2> " + $User_Desc + "</h2></div>" +
-        "<div class='inLineText'><h3>Department:  </h3> <h2> " + $User_Department + "</h2></div>"+
-        "<div class='inLineText'><h3>Job Title:  </h3> <h2> " + $User_JobTitle + "</h2></div>"+
-        "<div class='inLineText'><h3>Company:  </h3> <h2> " + $User_Company+ "</h2></div>" +
-        "<div class='inLineText'><h3>Office:  </h3> <h2> " + $User_Office+ "</h2></div>" +
-        "</div> "
+        <link href='https://fonts.googleapis.com/css2?family=Montserrat&display=swap' rel='stylesheet'>"
+        Title = "This is Description"
+        Body = "
+        <div class = 'title'> <h1>Generated by Corporate IT</h1></div> <div>"
+        PostContent = $assignToPostContent
+ 
       }
+
     #convert code to html file
     ConvertTo-Html @htmlParams |
     Out-File Show-User-Description.html
